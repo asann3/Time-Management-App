acd4a3167fb4f405ab22911c792c887795cb2001
asann3 <39186984+asann3@users.noreply.github.com>
2021-04-01 18:05:57 +0000

    npm run lint:fix

--- time-management-app/src/App.test.js
+++ time-management-app/src/App.test.js
@@ -1,3 +1,4 @@
+/* eslint-env jest */
 import { render, screen } from '@testing-library/react'
 import App from './App'
 
@@ -5,4 +6,4 @@ test('renders learn react link', () => {
   render(<App />)
   const linkElement = screen.getByText(/learn react/i)
   expect(linkElement).toBeInTheDocument()
-});
+})
